4235f0bfc4fecee05b13a0fcbeae32b5617a64a4
janefindlay <jane.findlay.16@gmail.com>
2025-04-24 18:57:20 +1200

    Test status api

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/status", (req, res) => {
+    const status = {
+      "Status": "Running"
+    }
+
+    res.send(status);
+});
+
+const server = app.listen(port, () => console.log(`Server listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
